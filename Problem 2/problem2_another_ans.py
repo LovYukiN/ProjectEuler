ae7faf60d999bb65cc6e5db3c2c295cc8c5d2bc0
LovYukiN <398836455@qq.com>
2017-08-25 12:24:00 +0800

    Create problem2_another_ans.py

---
+++ Problem 2/problem2_another_ans.py
@@ -0,0 +1,22 @@
+# Stealed from Project Euler thread 2 by Begoner
+# Explaination first
+'''
+This may be a small improvement.  The Fibonacci series is:
+
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610...
+
+Now, replacing an odd number with O and an even with E, we get:
+
+O, O, E, O, O, E, O, O, E, O, O, E, O, O, E...
+
+And so each third number is even.  We don't need to calculate the odd numbers.  Starting from an two odd terms x, y, the series is:
+
+x, y, x + y, x + 2y, 2x + 3y, 3x + 5y
+'''
+def calcE():
+	x = y = 1
+	sum = 0
+	while (sum < 1000000):
+		sum += (x + y)
+		x, y = x + 2 * y, 2 * x + 3 * y
+	return sum
